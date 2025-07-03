87e08c55130e38508e46c93e4a6d5f7faaaa35ba
Akshay Singh <114770287+Akshay-Singh11@users.noreply.github.com>
2025-07-03 13:10:47 +0530

    error correction on middleware

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -9,9 +9,7 @@ function RegisterPage() {
 
     const router = useRouter();
 
-
-
-    const handleSumit = async (e: React.FormEvent<HTMLFormElement>)=> {
+    const handleSumit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         if (password != confirmPassword) {
             alert("password do not match");
@@ -22,53 +20,55 @@ function RegisterPage() {
             // send registration request
             // react query ---> loading ,error ,data,debouncing and duplicate value
 
-            const respond = await fetch("/api/auth/register")
-            method: "POST",
+            const respond = await fetch("/api/auth/register", {
+                method: "POST",
                 headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ email, password }),
-            const data = await.respond.json();
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, password }),
+            });
+            const data = await respond.json();
             if (!respond.ok) {
                 throw new Error(data.error || "Registration failed");
             }
             console.log(data);
-            router.push("/login")
-        }
-        catch (error) {
+            router.push("/login");
+        } catch (error) {
             console.error(error);
         }
-    }
-    return <div>
-        <h1>Register Page</h1>
-        <form onSubmit={handleSumit}>
-            <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Email"
-                required
-            />
-            <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Password"
-                required
-            />
-            <input
-                type="password"
-                value={confirmPassword}
-                onChange={(e) => setConfirmPassword(e.target.value)}
-            />
-            <button type="submit">Register</button>
-        </form>
+    };
+    return (
         <div>
-            <p>
-                Already have an account? <a href="/login">Log in</a>
-            </p>
+            <h1>Register Page</h1>
+            <form onSubmit={handleSumit}>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Email"
+                    required
+                />
+                <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Password"
+                    required
+                />
+                <input
+                    type="password"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <button type="submit">Register</button>
+            </form>
+            <div>
+                <p>
+                    Already have an account? <a href="/login">Log in</a>
+                </p>
+            </div>
         </div>
-    </div>;
+    );
 }
 
 export default RegisterPage;
--- middleware.ts
+++ middleware.ts
@@ -8,17 +8,17 @@ export default withAuth(
     },
     {
         callbacks: {
-            authorized({ token }) {
-                const {pathname} =req.nextURL
+            authorized({ token, req }) {
+                const { pathname } = req.nextUrl;
                 if(
-                    pathname.startsWith("api/auth")||
-                    pathname === "/login"||
+                    pathname.startsWith("/api/auth") ||
+                    pathname === "/login" ||
                     pathname === "/register"
 
                 ){
                     return true;
                 }
-                if(pathname === "/" || pathname.startsWith("/api/videos")){
+                if(pathname === "/" || pathname.startsWith("/api/video")){
                     return true;
                 }
                 return !!token;
